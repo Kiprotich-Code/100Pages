a47fd80f99c330a7433b9af2a7f03756a42caa36
Lawi Kiprotich <fleeklawi@gmail.com>
2023-12-17 16:35:50 +0300

    Commit

--- main/users/views.py
+++ main/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+def signin(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('posts')
+        
+        else:
+            raise ValueError("User is not None")    
+    else: 
+        return render(request, 'users/login.html', {})
