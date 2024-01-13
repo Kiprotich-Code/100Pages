f8cdce5196e77307f4633d993ed115a330d88ffc
Lawi Kiprotich <fleeklawi@gmail.com>
2024-01-13 22:27:16 +0300

    Configuration for deployment to vercel

--- main/main/settings.py
+++ main/main/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-n5j8^%kr5%oej296t1fqat6qhavwx-ar-6y2ij2#wo*^81(i=n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vercel.app', 'now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -130,15 +130,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/'),
-]
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 MEDIA_URL = '/media/'
