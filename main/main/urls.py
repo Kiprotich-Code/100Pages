01f80f0402d72907c1a7190a032f26faef345316
Lawi Kiprotich <fleeklawi@gmail.com>
2024-06-09 16:15:41 +0300

    static settings

--- main/main/urls.py
+++ main/main/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('', include('users.urls')),
     path('summernote/', include('django_summernote.urls')),
 ]
-
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
