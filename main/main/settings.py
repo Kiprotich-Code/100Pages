b06b9dd26f5d08df0ae10bf136000cb3936eafb7
Lawi Kiprotich <fleeklawi@gmail.com>
2024-06-09 14:59:22 +0300

    error fix

--- main/main/settings.py
+++ main/main/settings.py
@@ -121,10 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    # add more directories if needed
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
