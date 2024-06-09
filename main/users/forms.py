14795295b2a3598c05015e454c0ed8dd60760592
Lawi Kiprotich <91130301+Kiprotich-Code@users.noreply.github.com>
2024-06-09 13:05:13 +0300

    Update forms.py

--- main/users/forms.py
+++ main/users/forms.py
@@ -14,7 +14,7 @@ class RegisterUserForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2', ]
         help_texts = {
             'username': None,
         }
