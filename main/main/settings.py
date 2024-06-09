a4a3cc119295d6905b3ec88913b2749ea4e7b71e
Lawi Kiprotich <fleeklawi@gmail.com>
2024-06-09 10:11:23 +0300

    adding db

--- main/main/settings.py
+++ main/main/settings.py
@@ -78,9 +78,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': 'PlWGAhxCEuBNAERGJhdfHrwDKnIaElZt',
+        'PASSWORD': 'RdCczORVVjcoimHQukivRWroKvgylSSR',
         'HOST': 'monorail.proxy.rlwy.net',
-        'PORT': '42197',
+        'PORT': '45272',
     }
 }
 
